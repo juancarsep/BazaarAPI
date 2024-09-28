832e2a98a3b1bec733a1aea42364295487de79b6
Juan Carballo <129075685+juancarsep@users.noreply.github.com>
2024-09-27 21:43:16 -0300

    Update BazarApplication.java

--- bazar/src/main/java/my/bazar/backend/bazar/BazarApplication.java
+++ bazar/src/main/java/my/bazar/backend/bazar/BazarApplication.java
@@ -10,4 +10,14 @@ public class BazarApplication {
 		SpringApplication.run(BazarApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI springDocOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Bazaar API")
+				.description("API de un Bazaar")
+				.version("0.0.1-DEV"))
+				.addServersItem(new Server().url("http://localhost:8080").description("Local"))
+				.addServersItem(new Server().url("http://ec2-18-117-218-252.us-east-2.compute.amazonaws.com:8080").description("AWS"))				
+			}
+	
 }
