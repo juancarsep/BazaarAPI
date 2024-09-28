baa94964a4c5707f80a464dbebf31dbac393de67
Juan Carballo <129075685+juancarsep@users.noreply.github.com>
2024-09-27 21:46:16 -0300

    Update BazarApplication.java

--- bazar/src/main/java/my/bazar/backend/bazar/BazarApplication.java
+++ bazar/src/main/java/my/bazar/backend/bazar/BazarApplication.java
@@ -17,7 +17,7 @@ public class BazarApplication {
 				.description("API de un Bazaar")
 				.version("0.0.1-DEV"))
 				.addServersItem(new Server().url("http://localhost:8080").description("Local"))
-				.addServersItem(new Server().url("http://ec2-18-117-218-252.us-east-2.compute.amazonaws.com:8080").description("AWS"))				
+				.addServersItem(new Server().url("http://ec2-18-117-218-252.us-east-2.compute.amazonaws.com:8080").description("AWS"));				
 			}
 	
 }
